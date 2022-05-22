2fd75371dfdda4e90ebe0ac30c8ae158567bbfa2
MagnusGeza <tamaskakics7@gmail.com>
2022-05-22 17:10:21 +0200

    Based project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {BrowserRouter, Route, Link, Routes} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Routes */}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
